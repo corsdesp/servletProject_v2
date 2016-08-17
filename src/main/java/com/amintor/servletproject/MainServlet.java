7d8f1f87211cb41a6ff6ce06e029db04397aae0c
corsdesp <corsar.k750i@gmail.com>
2016-08-17 15:49:38 +0300

    add web "Hello world"

--- src/main/java/com/amintor/servletproject/MainServlet.java
+++ src/main/java/com/amintor/servletproject/MainServlet.java
@@ -1,4 +1,21 @@
 package com.amintor.servletproject;
 
-public class MainServlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MainServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.print("<h1>Hello World<h1>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
 }
